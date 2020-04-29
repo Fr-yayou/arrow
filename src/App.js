8bb8bcb2619a181f4e8d8f720cf1c28dfa6ff358
Fr-yayou <yanis.bekhtaoui@gmail.com>
2020-04-29 12:40:41 -0700

    working arrow

--- src/App.js
+++ src/App.js
@@ -1,80 +1,75 @@
-import React, { Fragment } from 'react';
+import React, {Component } from 'react';
 import './App.css';
-import Toggle from './Toggle'
-import Modal from './Modal'
 import { ArcherContainer, ArcherElement } from 'react-archer';
+import {RIPES} from './data/Ripe'
 
-const boxStyle = { padding: '10px', border: '1px solid black' };
-const rootStyle = { display: 'flex', justifyContent: 'center' };
+const boxStyle = { padding: '10px', border: '1px solid black', borderRadius:50, width:'200px', backgroundColor:'#D3D3D3' };
+const rootStyle = { display: 'flex', justifyContent: 'center'  };
 const rowStyle = { marginTop:50, display: 'flex', justifyContent: 'space-between' }
 
 
-function App() {
-  return (
-    <div className="App">
-      <Modal/>
-      <Toggle>
-        {({ on, toggle }) => (
-          <Fragment>
-            <button onClick={toggle}>Show</button>
-            {
-             on && <Modal on={on} toggle={toggle} />
-            }
-          </Fragment>
+const Fetch = (props) =>{
+  return(
+    <ArcherContainer>
+    {props.pings.map((ping,index )=>{
+      console.log(index)
+        return(
+          <div  key={index} style={rootStyle}>
+            <ArcherElement id='root' relations={[{
+              targetId:'2',  
+              targetAnchor:'top',
+              sourceAnchor:'bottom',
+          }]} >
+            <div style={boxStyle}>Element {ping.from}</div>
+          </ArcherElement>
+          </div>
+        )
+        // return(
+        //   <div  key={ping.hop} style={rowStyle}>
+        //     <ArcherElement id={ping.hop} relations={[{
+        //       targetId:`${ping.hop}`,
+        //       targetAnchor:'bottom',
+        //       sourceAnchor:'bottom',
+        //       label:<div style={{marginLeft:'-50px'}}>rtt:{ping.rtt}</div>
 
-        )}
-      </Toggle>
-      <ArcherContainer strokeColor='yellow' >
-        <div style={rootStyle}>
-          <ArcherElement id="root1"
-            relations={[{
-              targetId: 'element2',
-              targetAnchor: 'top',
-              sourceAnchor: 'bottom',
-            },
-            {
-              targetId: 'element3',
-              targetAnchor: 'top',
-              sourceAnchor: 'bottom',
-            }
-            ]}
-          >
-          <div style={boxStyle}>Root 1</div>
-        </ArcherElement>
-        </div>
-        <div style={rowStyle}>
-          <ArcherElement id='element2'
-            relations={[{
-              targetId: 'element4',
-              targetAnchor: 'top',
-              sourceAnchor: 'bottom',
-            }]}
-          >
-            <div style={boxStyle}>Element 2</div>
-          </ArcherElement>
-          <ArcherElement id='element3'
-            relations={[{
-              targetId: 'element4',
-              targetAnchor: 'right',
-              sourceAnchor: 'left',
-            },
-            {
-              targetId: 'element2',
-              targetAnchor: 'right',
-              sourceAnchor: 'left',
-            }
-            ]}>
-            <div style={boxStyle}>Element 3</div>
-          </ArcherElement>
-        </div>
-        <div style={rowStyle}>
-          <ArcherElement id='element4' >
-            <div style={boxStyle}>Element 4</div>
-          </ArcherElement>
-        </div>
-      </ArcherContainer>
-    </div>
-  );
+        //     }]}>
+        //       <div style={boxStyle}>Elements{ping.ip}</div>
+        //   </ArcherElement>
+        //   </div>
+        // )
+    })}
+    
+  </ArcherContainer>
+  )
 }
 
-export default App;
+export default class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state={
+      pings:RIPES,
+
+    }
+  }
+
+// DATA FROM RIPE API//
+  // componentDidMount(){
+  //   fetch('http://localhost:8080')
+  //   .then(response => response.json())
+  //   .then(ping => this.setState({ping}))
+
+  // }
+
+  render(){
+
+
+    
+    return(
+      <div className="App">
+      <Fetch pings={this.state.pings}></Fetch>
+    </div>
+    )
+  }
+
+}
